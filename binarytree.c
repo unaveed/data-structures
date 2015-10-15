689a5129dec9895bfe5bed66a086c2b02a15a3d3
Umair Naveed <unaveed@gmail.com>
2015-10-15 03:42:55 -0600

    Started binary search tree

---
+++ binarytree.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+	int val;
+	struct node *left;
+	struct node *right;
+} b_node;
+
+b_node *root = NULL;
+
+int main(){
+	return 0;
+}
